7a079d030125d1d45450d401248875d3b27bccc5
Malu Abrami <marialuizaabrami@gmail.com>
2025-10-13 21:55:20 -0300

    feat: iniciando projeto

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      globals: globals.browser,
+    },
+    plugins: {
+      react,
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      react.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    rules: {
+      'react/react-in-jsx-scope': 'off', 
+      'react/prop-types': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn'],
+    },
+  },
+])
